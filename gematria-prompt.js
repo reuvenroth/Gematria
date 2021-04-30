d48f636a3a5e476adc8545ba1f268843c8cde924
Reuven <reuvenroth@users.noreply.github.com>
2021-04-30 01:32:54 -0400

    Update gematria-prompt.js
    All "vars" were updated to "lets". Some will likely be changed to const at a later date.

--- gematria-prompt.js
+++ gematria-prompt.js
@@ -1,11 +1,11 @@
-var input = prompt("Gematria!",  "Enter any Hebrew letters for their # value");
-var total = 0;
-var letters = ['א', 1, 'ב', 2, 'ג', 3, 'ד', 4, 'ה', 5, 'ו', 6, 'ז', 7, 'ח', 8, 'ט',  9, 'י', 10, 'כ', 20, 'ך', 20, 'ל', 30, 'מ', 40, 'ם', 40, 'נ', 50, 'ן', 50, 'ס', 60, 'ע', 70, 'פ', 80, 'ף', 80, 'צ', 90, 'ץ', 90, 'ק', 100, 'ר', 200, 'ש', 300, 'ת', 400, ' ', 0];
-var calc = function(input) {
-	for (var i = 0; i < input.length; i++) {
+let input = prompt("Gematria!",  "Enter any Hebrew letters for their # value");
+let total = 0;
+let letters = ['א', 1, 'ב', 2, 'ג', 3, 'ד', 4, 'ה', 5, 'ו', 6, 'ז', 7, 'ח', 8, 'ט',  9, 'י', 10, 'כ', 20, 'ך', 20, 'ל', 30, 'מ', 40, 'ם', 40, 'נ', 50, 'ן', 50, 'ס', 60, 'ע', 70, 'פ', 80, 'ף', 80, 'צ', 90, 'ץ', 90, 'ק', 100, 'ר', 200, 'ש', 300, 'ת', 400, ' ', 0];
+let calc = function(input) {
+	for (let i = 0; i < input.length; i++) {
 	console.log(i);
 	console.log(input[i]);
-		for (var j = 0; j < letters.length; j+=2) {
+		for (let j = 0; j < letters.length; j+=2) {
 			if(input[i] === letters[j]) {
 				total += letters[j+1];
 			};
@@ -16,7 +16,7 @@ var calc = function(input) {
 };
 total = calc(input);
 alert("The total of " + total + " is your Gematria.");
-var again = confirm("Would you like to check another Gematria?");
+let again = confirm("Would you like to check another Gematria?");
 while (again === true) {
 	total = 0;
 	input = prompt("Enter any Hebrew letters for their # value");
@@ -24,4 +24,4 @@ while (again === true) {
 	alert("The total of " + total + " is your Gematria.");
 	again = confirm("Would you like to check another Gematria?");
 };
-//* אבגדהוזחטיכךלמםנןסעפףצץקרשת 
+//* אבגדהוזחטיכךלמםנןסעפףצץקרשת
